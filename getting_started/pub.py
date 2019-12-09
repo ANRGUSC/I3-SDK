ac3936ef1bf5662de730d6a754ac078e9525a472
rbpandya <rutulpandya2009@gmail.com>
2019-12-09 15:21:39 -0800

    adding readme and changing file names

--- getting_started/pub.py
+++ getting_started/pub.py
@@ -2,27 +2,16 @@ import paho.mqtt.client as mqtt
 import time
 import os
 
-def on_connect(client, userdata, flags, rc):    
-    print("Result from connect: {}".format(mqtt.connack_string(rc)))
-    client.subscribe(topic[0])
-
-
-def on_subscribe(client, userdata, mid, granted_qos):    
-    print("Subscribed")
-
-
-def on_message(client, userdata, msg):
-    print("Message received. Topic: {}. Payload: {}".format(msg.topic, str(msg.payload)))
 
 if __name__ == "__main__":
 
     #TODO: add details here for connection
-    #account refers to username on i3 Marketplace of the subscriber
-    #pw referes to password obtained in the notification tray
+    #account refers to full device name under the hubs section of the topic 
+    #pw referes to password added while creating device (API_key and asymmetric public key)
     #host refers to MQTT broker
-    account = 'muser2'
+    account = 'muser1$testhub1996$device77'
     topic = ['muser1/testhub1996/topic1996']
-    pw = 'xgndw220kwuh'
+    pw = 'polish7'
     port = 1883
     host = '18.219.4.146'
 
@@ -34,18 +23,17 @@ try:
             fread.close()
         if host == 'Default' or port == 'Default' or topic == 'Default' or account == 'Default':
             print ("ERROR: Check host, topic, account and password values")
-            print ("The account is the username of the subscriber on i3 Marketplace")
+            print ("The account is the device name registered under the hub containing the topic on i3 Marketplace")
             print ("The topic is the full-name of the product which is purchased from the I3 Data market place")
             print ("The password is the system generated password when the product is purchased")
             raise Exception(" Default values not changed ")
 
-        client = mqtt.Client(protocol=mqtt.MQTTv311)    
-        client.on_connect = on_connect    
-        client.on_subscribe = on_subscribe    
-        client.on_message = on_message
+        client = mqtt.Client(protocol=mqtt.MQTTv311)
         client.username_pw_set(account, pw)
-        client.connect(host, port)   
-        client.loop_forever()
+        client.connect(host, port)
+        print("Connected to broker") 
+        client.publish(topic[0], payload="I3 Marketplace")
+        print("Published")
 
 except Exception as e:
     print ("Exception" + str(e))
